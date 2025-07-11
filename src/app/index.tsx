fad458acc694d9130545718f7d9424706c1f2098
Sthefane Silva <sthefanedias04@gmail.com>
2025-07-11 17:00:30 -0300

    configurações iniciais react native

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white">Hello World</Text>
+    </View>
+  );
+}
