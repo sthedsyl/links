0e213c77b4ef7f327818fb4fb2e54d000b830576
Sthefane Silva <sthefanedias04@gmail.com>
2025-07-14 17:38:40 -0300

    feat: estilização e adicao de botao

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
-import {Text, View} from "react-native";
+import {Text,View, Pressable} from "react-native";
 import "../global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white">Hello World</Text>
-    </View>
+      <View className="bg-white p-4 rounded shadow-md mt-10">
+        <Text className="text-lg font-bold">Bem-vindo ao App!</Text>
+        <Text className="text-black">Hello World</Text>
+        <Pressable className="bg-blue-500 px-4 py-4 rounded mt-4" onPress={() => alert("Botão pressionado!")}>
+          <Text className="text-white">Clique aqui!</Text>
+        </Pressable>
+      </View>
   );
 }
